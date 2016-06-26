543f2b45c18bd9f0c4e8df3b9b0b60bae17a5ba9
admin <olegbochkarev@macbook-pro-oleg.local>
2016-06-26 16:07:17 +0300

    Initial Commit

---
+++ TrigerViewTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TrigerViewTest
+//
+//  Created by Oleg Bochkarev on 26.06.16.
+//  Copyright © 2016 Oleg Bochkarev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
